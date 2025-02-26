a59e35e84967d40ae71220e904f7c3bcbdf04804
Jared Hatfield <jjhatf02@gmail.com>
2025-02-25 20:11:44 -0500

    Add YAML test files and update test function for comparison logic

--- yamlequal_test.go
+++ yamlequal_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestEqual(t *testing.T) {
+func TestYAMLFiles(t *testing.T) {
 	testDirs, err := os.ReadDir("tests")
 	if err != nil {
 		t.Fatalf("Error reading tests directory: %v", err)
@@ -16,12 +16,17 @@ func TestEqual(t *testing.T) {
 		if !dir.IsDir() {
 			continue
 		}
-
 		dirPath := filepath.Join("tests", dir.Name())
-
 		t.Run(dir.Name(), func(t *testing.T) {
 			file1Path := filepath.Join(dirPath, "file1.yaml")
 			file2Path := filepath.Join(dirPath, "file2.yaml")
+			notEqualFlagPath := filepath.Join(dirPath, "notequal.flag")
+
+			// Check if files should be not equal
+			expectEqual := true
+			if _, err := os.Stat(notEqualFlagPath); err == nil {
+				expectEqual = false
+			}
 
 			// If files don't exist test fails
 			if _, err := os.Stat(file1Path); os.IsNotExist(err) {
@@ -31,12 +36,17 @@ func TestEqual(t *testing.T) {
 				t.Fatalf("File %s does not exist", file2Path)
 			}
 
-			equal, _, err := CompareYAMLFiles(file1Path, file2Path)
+			equal, diff, err := CompareYAMLFiles(file1Path, file2Path)
 			if err != nil {
 				t.Fatalf("Error comparing YAML files in %s: %v", dirPath, err)
 			}
-			if !equal {
-				t.Fatalf("Files %s and %s are not equal", file1Path, file2Path)
+
+			if expectEqual && !equal {
+				t.Fatalf("Expected files to be equal, but they were not. Diff: %s", diff)
+			}
+
+			if !expectEqual && equal {
+				t.Fatalf("Expected files to be different, but they were equal")
 			}
 		})
 	}
